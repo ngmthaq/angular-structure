8dd5576c3a246f7dcfc73a8f67f05bd228a94503
Nguyễn Mạnh Thắng <nguyenmanhthang2000.fb@gmail.com>
2024-05-26 09:11:07 +0700

    Init e2e testing

---
+++ e2e/tests/app.test.ts
@@ -0,0 +1,13 @@
+import * as assert from "assert";
+import { describe, it } from "node:test";
+import { setupBrowserHooks, getBrowserState } from "./utils";
+
+describe("App test", function () {
+  setupBrowserHooks();
+  it("is running", async function () {
+    const { page } = getBrowserState();
+    const element = await page.locator("::-p-text(angular-structure)").wait();
+
+    assert.ok(element);
+  });
+});
