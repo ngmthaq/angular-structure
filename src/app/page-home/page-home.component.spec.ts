5635102bb4cb294374564547d597b00314496dd5
Nguyễn Mạnh Thắng <nguyenmanhthang2000.fb@gmail.com>
2024-05-26 08:34:21 +0700

    Fix unit testing

--- src/app/page-home/page-home.component.spec.ts
+++ src/app/page-home/page-home.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { TranslateModule } from "@ngx-translate/core";
 
 import { PageHomeComponent } from "./page-home.component";
 
@@ -9,6 +10,7 @@ describe("PageHomeComponent", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [PageHomeComponent],
+      imports: [TranslateModule.forRoot()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PageHomeComponent);
